19191ecc264193f9d5364efb903332ab17fb5d5c
Jakub Ciechowski <sianex@gmail.com>
2017-08-10 09:57:01 +0200

    events service

---
+++ src/app/events-list/events.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Event, Events } from './events';
+
+@Injectable()
+export class EventsService {
+  events: Event[];
+
+  constructor() {
+    this.events = Events;
+  }
+
+  getEvents(): Promise<Event[]> {
+    return Promise.resolve(this.events);
+  };
+}
