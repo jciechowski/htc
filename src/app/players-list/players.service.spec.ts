7fd820839e449fe180b081fab17953cf8b4005d2
Jakub Ciechowski <sianex@gmail.com>
2017-08-12 10:55:43 +0200

    players service test

--- src/app/players-list/players.service.spec.ts
+++ src/app/players-list/players.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { PlayersService } from './players.service';
+import { Gender, Players } from './players';
 
 describe('PlayersService', () => {
   beforeEach(() => {
@@ -11,5 +12,16 @@ describe('PlayersService', () => {
 
   it('should be created', inject([PlayersService], (service: PlayersService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should get all players', inject([PlayersService], (service: PlayersService) => {
+    const allPlayers = service.getPlayers();
+    expect(allPlayers).toEqual(Players);
+  }));
+
+  it('should add new player', inject([PlayersService], (service: PlayersService) => {
+    const totalPLayers = service.getPlayers().length;
+    service.addPlayer({name: 'Test', lastname: 'Test', jerseyNumber: 0, gender: Gender.man});
+    expect(service.getPlayers().length).toEqual(totalPLayers + 1);
   }));
 });
