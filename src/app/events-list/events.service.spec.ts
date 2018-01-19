75989a16642429588505cd18e3e696f065c6f921
Jakub Ciechowski <sianex@gmail.com>
2018-01-19 15:24:05 +0100

    removed deprecated tests - need to write some

--- src/app/events-list/events.service.spec.ts
+++ src/app/events-list/events.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { EventsService } from './events.service';
-import { TeamEvent, TeamEvents } from './events';
+import { TeamEvent } from './events';
 import { Observable } from 'rxjs/Observable';
 import { PlayersService } from 'app/players-list/players.service';
 
@@ -16,28 +16,6 @@ describe('EventsService', () => {
     'should be created',
     inject([EventsService], (service: EventsService) => {
       expect(service).toBeTruthy();
-    })
-  );
-
-  it(
-    'should return events',
-    inject([EventsService], (service: EventsService) => {
-      expect(service.getEvents()).toEqual(TeamEvents);
-    })
-  );
-
-  it(
-    'should add new event',
-    inject([EventsService], (service: EventsService) => {
-      const beforeEventsLength = service.getEvents().length;
-      const newEvent: TeamEvent = {
-        title: 'Test Event',
-        place: 'Some place',
-        date: new Date('01/13/2017'),
-        attendance: { man: 0, woman: 0, tbd: 2 }
-      };
-      service.addEvent(newEvent);
-      expect(service.getEvents().length).toEqual(beforeEventsLength + 1);
     })
   );
 });
--- src/app/players-list/players.service.spec.ts
+++ src/app/players-list/players.service.spec.ts
@@ -10,18 +10,10 @@ describe('PlayersService', () => {
     });
   });
 
-  it('should be created', inject([PlayersService], (service: PlayersService) => {
-    expect(service).toBeTruthy();
-  }));
-
-  it('should get all players', inject([PlayersService], (service: PlayersService) => {
-    const allPlayers = service.getPlayers();
-    expect(allPlayers).toEqual(Players);
-  }));
-
-  it('should add new player', inject([PlayersService], (service: PlayersService) => {
-    const totalPLayers = service.getPlayers().length;
-    service.addPlayer({name: 'Test', lastname: 'Test', jerseyNumber: 0, gender: Gender.man});
-    expect(service.getPlayers().length).toEqual(totalPLayers + 1);
-  }));
+  it(
+    'should be created',
+    inject([PlayersService], (service: PlayersService) => {
+      expect(service).toBeTruthy();
+    })
+  );
 });
