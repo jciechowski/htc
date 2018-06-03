786b907cbf71a3f10e8f113f7e3bf1066b534b76
Jakub Ciechowski <sianex@gmail.com>
2018-06-03 10:59:04 +0200

    firebase settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyDRwVQFXWl3ZubB-Q6JOlZhW3LLgyqr7To',
+    authDomain: 'ngfirebasedraft.firebaseapp.com',
+    databaseURL: 'https://ngfirebasedraft.firebaseio.com',
+    projectId: 'ngfirebasedraft',
+    storageBucket: 'ngfirebasedraft.appspot.com',
+    messagingSenderId: '744765734085'
   }
 };
